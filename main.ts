81c709baa7126afcef5f890742f2093e4df47e1e
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2026-01-12 12:25:08 -0500

    sender

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2026 Isaac Ip All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Isaac Ip
+ * Created on: Jan 2026
+ * This program sends a signal to another microbit
 */
-
-basic.showString('Hello, World!')
+radio.setGroup(196)
+input.onButtonPressed(Button.A, function() {
+    radio.sendNumber(0)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(1)
+})
