0fd49a0d9a68ee59978d1e6aabfd76f5c96d6293
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2026-01-19 13:10:20 -0500

    made sounds

--- main.ts
+++ main.ts
@@ -3,11 +3,34 @@
  * Created by: Isaac Ip
  * Created on: Jan 2026
  * This program sends a signal to another microbit
-*/
+ */
+
+// variables
+let neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+const servoNumber1 = robotbit.Servos.S1
 radio.setGroup(196)
-input.onButtonPressed(Button.A, function() {
-    radio.sendNumber(0)
+music.setVolume(255)
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(11)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
+    music.play(music.stringPlayable("G - G - G - - - ", 800), music.PlaybackMode.UntilDone)
 })
+
 input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(1)
+    radio.sendNumber(22)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.show()
+    music.play(music.stringPlayable("C5 - C5 - C5 - - - ", 800), music.PlaybackMode.UntilDone)
 })
